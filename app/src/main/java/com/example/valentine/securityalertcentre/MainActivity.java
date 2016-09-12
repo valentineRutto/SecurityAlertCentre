c0d6bd447b2573afd67ad5618f13f6ba224d287b
valentinerutto <rutto28@gmail.com>
2016-09-12 20:55:27 +0300

    populated strings file

--- app/src/main/java/com/example/valentine/securityalertcentre/MainActivity.java
+++ app/src/main/java/com/example/valentine/securityalertcentre/MainActivity.java
@@ -5,8 +5,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
+import android.view.View;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
