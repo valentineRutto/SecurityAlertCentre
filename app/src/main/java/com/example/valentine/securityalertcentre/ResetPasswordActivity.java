75f68e639b38e01f9dc10dd6873febfdb83c5ccb
valentinerutto <rutto28@gmail.com>
2016-09-12 23:24:00 +0300

    populated resetpassword layout

--- app/src/main/java/com/example/valentine/securityalertcentre/ResetPasswordActivity.java
+++ app/src/main/java/com/example/valentine/securityalertcentre/ResetPasswordActivity.java
@@ -1,7 +1,13 @@
 package com.example.valentine.securityalertcentre;
 
-/**
- * Created by valentine on 9/12/16.
- */
-public class ResetPasswordActivity {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ResetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+    }
 }
