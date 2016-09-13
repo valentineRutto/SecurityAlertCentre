10664099dc167ffdc8a4805f51edc1f4b015ebd5
valentinerutto <rutto28@gmail.com>
2016-09-13 03:27:20 +0300

    populated main activity

--- app/src/main/java/com/example/valentine/securityalertcentre/MainActivity.java
+++ app/src/main/java/com/example/valentine/securityalertcentre/MainActivity.java
@@ -4,11 +4,18 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.GestureDetector;
 import android.view.Menu;
+import android.view.MotionEvent;
 import android.view.View;
 
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.valentine.securityalertcentre.Adapter.DataAdapter;
 
 import java.util.ArrayList;
 
@@ -34,7 +41,51 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initViews() {
+        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.card_recycler_view);
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        alerts = new ArrayList<>();
+        alerts.add("Australia");
+        alerts.add("India");
+        alerts.add("United States of America");
+        alerts.add("Germany");
+        alerts.add("Russia");
+        final RecyclerView.Adapter adapter = new DataAdapter(alerts);
+        recyclerView.setAdapter(adapter);
+
+        recyclerView.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
+            GestureDetector gestureDetector = new GestureDetector(getApplicationContext(), new GestureDetector.SimpleOnGestureListener() {
+
+                @Override public boolean onSingleTapUp(MotionEvent e) {
+                    return true;
+                }
+
+            });
+            @Override
+            public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+
+                View child = rv.findChildViewUnder(e.getX(), e.getY());
+                if(child != null && gestureDetector.onTouchEvent(e)) {
+                    int position = rv.getChildAdapterPosition(child);
+                    Toast.makeText(getApplicationContext(), alerts.get(position), Toast.LENGTH_SHORT).show();
+                }
+
+                return false;
+            }
+
+            @Override
+            public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+            }
+
+            @Override
+            public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+            }
+        });
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
